39564b9a7af1b7753f667424a23b1152a42817bc
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-03-17 01:53:13 +0530

    Initialised DailyFlash 2

---
+++ dailyflash2/lib/assignment1.dart
@@ -0,0 +1,39 @@
+//container with rounded red colored borders and Text if center of container
+import 'package:flutter/material.dart';
+
+class Assignment1 extends StatefulWidget {
+  const Assignment1({super.key});
+
+  @override
+  State<Assignment1> createState() => _Assignment1State();
+}
+
+class _Assignment1State extends State<Assignment1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Assignment 1"),
+        centerTitle: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(15),
+            bottomRight: Radius.circular(15),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.redAccent,
+              width: 5,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
