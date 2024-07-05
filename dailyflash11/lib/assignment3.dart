cfa41e70d86698ce27138aed0305c7452a0cd589
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-07-06 01:16:43 +0530

    Assignment 3 updated

--- dailyflash11/lib/assignment3.dart
+++ dailyflash11/lib/assignment3.dart
@@ -1,1 +1,55 @@
+import 'package:flutter/material.dart';
 
+class Assignment3 extends StatefulWidget {
+  const Assignment3({super.key});
+
+  @override
+  State createState() => _Assignment3State();
+}
+
+class _Assignment3State extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Assignment 3",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          width: 250,
+          padding: const EdgeInsets.all(10),
+          child: TextField(
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.amber,
+              hintText: "Enter your name",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: const BorderSide(color: Colors.black),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: const BorderSide(color: Colors.black),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
