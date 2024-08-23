797a0910a85dc7ce20278285d523ea543310c460
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-08-23 21:08:27 +0530

    Imported files

--- dailyflash13/lib/main.dart
+++ dailyflash13/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:dailyflash13/assignment2.dart';
 import 'package:flutter/material.dart';
-import 'assignment1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Assignment1(),
+    return const MaterialApp(
+      home: Assignment2(),
       debugShowCheckedModeBanner: false,
     );
   }
