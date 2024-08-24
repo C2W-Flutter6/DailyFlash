ed3e51b629aa96e79de9c52541f606fbc24487e9
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-08-24 19:51:17 +0530

    Email Validation assignment

---
+++ dailyflash13/lib/assignment3.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+class Assignment3 extends StatefulWidget {
+  const Assignment3({super.key});
+
+  @override
+  State<Assignment3> createState() => _Assignment3State();
+}
+
+class _Assignment3State extends State<Assignment3> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+
+  void validateData() {
+    if (_formKey.currentState?.validate() == true) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          backgroundColor: Color.fromARGB(255, 114, 208, 246),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+            ),
+          ),
+          content: Text(
+            textAlign: TextAlign.center,
+            'Successful',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Color.fromARGB(255, 6, 63, 109),
+            ),
+          ),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          backgroundColor: Color.fromARGB(255, 114, 208, 246),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+            ),
+          ),
+          content: Text(
+            textAlign: TextAlign.center,
+            'Enter a valid email address.',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Color.fromARGB(255, 6, 63, 109),
+            ),
+          ),
+        ),
+      );
+    }
+  }
+
+  String? emailValidator(String? value) {
+    final RegExp emailExp = RegExp(r'^[a-z0-9._%+-]+@gmail\.com$');
+    if (value == null ||
+        !emailExp.hasMatch(value) ||
+        value != value.toLowerCase()) {
+      return 'Enter a valid email address ending with @gmail.com in lowercase.';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        title: const Text(
+          "Form Validation",
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.w500, color: Colors.white),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Email Field
+              Container(
+                width: 300,
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color.fromARGB(255, 240, 244, 248),
+                ),
+                child: TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 53, 52, 52),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        width: 1.5,
+                        color: Color.fromARGB(255, 30, 30, 30),
+                      ),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 244, 102, 92),
+                        width: 1.5,
+                      ),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 238, 115, 107),
+                        width: 1.5,
+                      ),
+                    ),
+                  ),
+                  validator: emailValidator,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              // Submit Button
+              SizedBox(
+                width: 100,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: validateData,
+                  child: const Text(
+                    "Submit",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color.fromARGB(255, 13, 13, 14),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
