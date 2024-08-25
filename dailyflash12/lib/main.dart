25410087fd593d0ffc50b5909df939fbbbfce4f1
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-08-25 23:18:59 +0530

    Imported assignment file

--- dailyflash12/lib/main.dart
+++ dailyflash12/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Assignment4.dart';
+import 'Assignment5.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Assignment4(),
+      home: Assignment5(),
       debugShowCheckedModeBanner: false,
     );
   }
