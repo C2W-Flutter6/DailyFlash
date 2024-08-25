2793759958d1cd8f7d73a8e58fe4511c95e630f6
C2W-Flutter6 <flutterkhushi@gmail.com>
2024-08-25 23:18:43 +0530

    appbar added

--- dailyflash12/lib/Assignment5.dart
+++ dailyflash12/lib/Assignment5.dart
@@ -1,1 +1,33 @@
 import 'package:flutter/material.dart';
+
+class Assignment5 extends StatefulWidget {
+  const Assignment5({super.key});
+
+  @override
+  State<Assignment5> createState() => _Assignment5State();
+}
+
+class _Assignment5State extends State<Assignment5> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 122, 184, 234),
+        title: const Text(
+          "DatePicker",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+}
